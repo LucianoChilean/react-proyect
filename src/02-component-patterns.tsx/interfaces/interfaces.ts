38ce34f534ae00f395d31f9ed9a12ee5dd824c13
Luciano Saavedra <lsaaved5@externos.bancoestado.cl>
2024-02-15 01:32:33 -0300

    component compound listo

---
+++ src/02-component-patterns.tsx/interfaces/interfaces.ts
@@ -0,0 +1,31 @@
+import { ReactElement } from "react";
+
+
+export interface ProductCardProps {
+    product: Product;
+    children?: ReactElement | ReactElement[]
+}
+
+export interface Product {
+    id: string;
+    title: string;
+    img?: string;
+}
+
+export interface ProductContextProps {
+    counter: number;
+    increaseBy: (value: number) => void;
+    product: Product;
+}
+
+export interface ProductButtonsProps {
+    increaseBy: (value: number) => void,
+    counter: number
+}
+
+export interface ProductCardHOCProps {
+    ({ children, product }: ProductCardProps): JSX.Element,
+    Title: ({ title }: { title?: string }) => JSX.Element,
+    Image: ({ img }: { img?: string }) => JSX.Element,
+    Buttons: () => JSX.Element,
+}
---
+++ src/routes/Navigation.tsx
@@ -0,0 +1,40 @@
+import {
+    BrowserRouter,
+    Route,
+    NavLink,
+    Routes
+} from 'react-router-dom';
+
+import logo from '../logo.svg';
+import { ShoppingPage } from '../02-component-patterns.tsx/pages/ShoppingPage';
+
+export const Navigation = () => {
+    return (
+        <BrowserRouter>
+            <div className="main-layout">
+                <nav>
+                    <img src={logo} alt="React Logo" />
+                    <ul>
+                        <li>
+                            <NavLink to="/" className={({ isActive }) => isActive ? 'nav-active' : ''}>Shopping</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/about" className={({ isActive }) => isActive ? 'nav-active' : ''}>About</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/users" className={({ isActive }) => isActive ? 'nav-active' : ''}>Users</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+
+                {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+                <Routes>
+                    <Route path="/about" element={<h1>About</h1>} />
+                    <Route path="/users" element={<h1>Users</h1>} />
+                    <Route path="/" element={<ShoppingPage />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
+}
